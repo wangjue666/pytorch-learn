f6480ce0b230c0e54aa65e04fbfd7e84d4c53f26
王珏 <wangjuesix@gmail.com>
2021-03-24 18:35:21 +0800

    引入呈现气温数据集

---
+++ temperature-predict/index.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+import torch
+import torch.optim as optim
+import warnings
+warnings.filterwarnings("ignore")
+
+
+
+# 引入模型
+features = pd.read_csv('./temperature-predict/temps.csv')
+# 看看数据长什么样子
+print(features.head())
+print('数据维度:', features.shape)
+
+# 处理时间数据
+import datetime
+
+# 分别得到年，月，日
+years = features['year']
+months = features['month']
+days = features['day']
+
+# datetime格式
+dates = [str(int(year)) + '-' + str(int(month)) + '-' + str(int(day)) for year, month, day in zip(years, months, days)]
+dates = [datetime.datetime.strptime(date, '%Y-%m-%d') for date in dates]
+
+
+
+# 准备画图
+# 指定默认风格
+plt.style.use('fivethirtyeight')
+
+# 设置布局
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2, ncols=2, figsize = (10,10))
+fig.autofmt_xdate(rotation = 45)
+
+# 标签值
+ax1.plot(dates, features['actual'])
+ax1.set_xlabel(''); ax1.set_ylabel('Temperature'); ax1.set_title('Max Temp')
+
+# 昨天
+ax2.plot(dates, features['temp_1'])
+ax2.set_xlabel(''); ax2.set_ylabel('Temperature'); ax2.set_title('Previous Max Temp')
+
+# 前天
+ax3.plot(dates, features['temp_2'])
+ax3.set_xlabel('Date'); ax3.set_ylabel('Temperature'); ax3.set_title('Two Days Prior Max Temp')
+
+# 朋友预测
+ax4.plot(dates, features['friend'])
+ax4.set_xlabel('Date'); ax4.set_ylabel('Temperature'); ax4.set_title('Friend Estimate')
+
+plt.tight_layout(pad=2)
+plt.show()
